d1d370a34db5c1b607248885d72d7a6856bc4ebb
Jürgen Bergmann <bergmannj@posteo.de>
2023-05-24 10:41:18 +0200

    fix: remove react-native-image-transformer

--- src/screens/Wagonimage.tsx
+++ src/screens/Wagonimage.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RouteProp } from '@react-navigation/native';
 import { MainStackParamList, WagonimageScreenParams } from './ScreenTypes';
-import ImageTransformer from "react-native-image-transformer";
+import { Dimensions, Image } from 'react-native';
+import { useOrientation } from './useOrientation';
 
 type Props = {
     route: RouteProp<MainStackParamList, 'Wagonimage'>;
@@ -16,16 +17,19 @@ export default function WagonimageScreen({ route }: Props): JSX.Element {
     const image = params.image;
     const uri = 'https://lib.finalrewind.org/dbdb/db_wagen/' + image + '.png';
 
-    console.log('WagonimageScreen: ', uri);
-
+    const dim = Dimensions.get('window');
+    console.log('WagonimageScreen: ', uri, dim);
+    const orientation = useOrientation();
     return (
-
-        <ImageTransformer
-            style={{ flex: 1, paddingLeft: 5, paddingRight: 5 }}
-            image={
-                {
-                    uri: uri
-                }
-            } />
+        orientation === 'PORTRAIT' ?
+            <Image
+                style={{ marginTop: dim.height / 2 - 110, marginLeft: 10, marginRight: 10, width: dim.width - 20, height: 50 }}
+                source={{ uri: uri }}
+            />
+            :
+            <Image
+                style={{ marginTop: dim.height / 2 - 110, marginLeft: 10, marginRight: 10, width: dim.width - 20, height: 90 }}
+                source={{ uri: uri }}
+            />
     );
 }
